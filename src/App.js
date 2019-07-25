bfc292942e6be11e122f87bcc41f8206eecb6e7c
ChrisBurch <christian.burchhardt@gmail.com>
2019-07-25 11:22:14 -0700

    reproducing webstorm syntax highlighting issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import makeStyles from "@material-ui/styles/makeStyles";
+import React from "react";
+import "./App.css";
+import { useOtherStyles } from "./styles";
+
+const useStyles = makeStyles({
+  test: {
+    backgroundColor: "blue"
+  }
+});
 
 function App() {
+  const classes = useStyles();
+  const otherClasses = useOtherStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/*In Webstorm 2019.2, this highlights correctly */}
+      <div className={classes.test}>Test1</div>
+      {/*In Webstorm 2019.2, this highlights in yellow, saying "Unresolved variable test2" */}
+      <div className={otherClasses.test2}>Test2</div>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,9 @@
+import makeStyles from "@material-ui/styles/makeStyles";
+
+const useOtherStyles = makeStyles({
+  test2: {
+    backgroundColor: "red"
+  }
+});
+
+export { useOtherStyles };
